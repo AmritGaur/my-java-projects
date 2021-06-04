a30032e75526d2683c8472e20d36d4e827868d6c
AmritGaur <84451762+AmritGaur@users.noreply.github.com>
2021-06-04 21:50:03 +0530

    Add files via upload

---
+++ DataTypeTutorials.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class DataTypeTutorials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       byte x = 33;
+       byte y = 121;
+      int m=x+y;
+       System.out.println(m);
+	}
+
+}
---
+++ DataTypesSecond.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class DataTypesSecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float rate= 12.898899f;
+     byte time = 5;
+     short principal=15000;
+     double simpleinterest= rate*time*principal/100; 
+    		 System.out.println(simpleinterest);
+    		 boolean indiawonseriesagainstaustralia = true;
+    		 System.out.println(indiawonseriesagainstaustralia);
+	}
+
+}
